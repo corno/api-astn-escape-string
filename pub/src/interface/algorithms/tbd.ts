fc70690d5fc1d086f873b620e375561d08941ce7
corno <corno.astn.a@schraverus.com>
2022-09-06 01:53:06 +0200

    .

---
+++ pub/src/interface/algorithms/tbd.ts
@@ -0,0 +1,17 @@
+import * as pt from "pareto-core-types"
+
+export type FEscapeString = (
+    $: {
+        readonly "str": string,
+        readonly "escapeTabsAndNewLines": boolean,
+        readonly "wrapperToEscape": null | string,
+    }
+) => string
+
+
+export type FEscapeMultilineString = (
+    $: {
+        readonly "lines": pt.Array<string>,
+        readonly "indentation": string,
+    }
+) => string
