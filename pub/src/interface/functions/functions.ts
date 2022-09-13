6f31d78de4b60245361c5b126f35a258acc02d19
corno <corno.astn.a@schraverus.com>
2022-09-13 17:27:19 +0200

    .

--- pub/src/interface/functions/functions.ts
+++ pub/src/interface/functions/functions.ts
@@ -3,15 +3,15 @@ import * as pt from "pareto-core-types"
 export type FEscapeString = (
     $: {
         readonly "str": string,
-        readonly "escapeTabsAndNewLines": boolean,
-        readonly "wrapperToEscape": null | string,
+        readonly "escapeTabsAndNewLines": boolean
+        readonly "wrapperToEscape": null | string
     }
 ) => string
 
 
 export type FEscapeMultilineString = (
     $: {
-        readonly "lines": pt.Array<string>,
-        readonly "indentation": string,
+        readonly "lines": pt.Array<string>
+        readonly "indentation": string
     }
 ) => string
